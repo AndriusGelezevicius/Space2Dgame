8253ccf16d3fafaf13197ba25e553abd8968ff25
AndriusGelezevicius <andriusgelezevicius@gmail.com>
2023-11-22 18:40:31 +0200

    made that new rocket's position will be within the bounds

--- src/main/java/Software/MainGui.java
+++ src/main/java/Software/MainGui.java
@@ -18,17 +18,20 @@ public class MainGui implements KeyListener {
     ImageIcon asteroidIcon;
     ImageIcon backgroundImage;
     Timer timer;
+    int jFrameWidth = 500;
+    int jFrameHeight = 500;
 
     public void drawingFrame(){
         jFrame = new JFrame();
-        jFrame.setSize(500,500);
+        jFrame.setSize(jFrameWidth,jFrameHeight);
         jFrame.setLayout(null);
         jFrame.addKeyListener(this);
         jFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        jFrame.setResizable(false);
         jFrame.setLocationRelativeTo(null);
         rocketIcon = new ImageIcon("src/rocket (1).png");
         asteroidIcon = new ImageIcon("src/asteroid.png");
-        backgroundImage = new ImageIcon("src/background.png");
+
 
         rocketJLabel = new JLabel();
         rocketJLabel.setBounds(210,220,400,400);
@@ -40,24 +43,11 @@ public class MainGui implements KeyListener {
         jFrame.add(asteroidJLabel);
         jFrame.add(rocketJLabel);
 
-//:TODO something wrong
-        timer = new Timer(500, new ActionListener(){
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                Random random = new Random();
-                int newX = random.nextInt(jFrame.getWidth() - asteroidJLabel.getWidth());
-                int newY = random.nextInt(jFrame.getHeight() - asteroidJLabel.getHeight());
-
-                asteroidJLabel.setLocation(newX, newY);
-            }
-        });
-        timer.start();
         jFrame.setVisible(true);
 
+        
+
     }
-
-
-
 
 
     @Override
@@ -67,19 +57,43 @@ public class MainGui implements KeyListener {
     public void keyPressed(KeyEvent e) {
         switch (e.getKeyCode()){
             case 38:
-                rocketJLabel.setLocation(rocketJLabel.getX(), rocketJLabel.getY()-10);
+                moveRocket(0,-10);
                 break;
             case 40:
-                rocketJLabel.setLocation(rocketJLabel.getX(), rocketJLabel.getY()+10);
+                moveRocket(0,+10);
                 break;
             case 37:
-                rocketJLabel.setLocation(rocketJLabel.getX()-10, rocketJLabel.getY());
+                moveRocket(-10,0);
                 break;
             case 39:
-                rocketJLabel.setLocation(rocketJLabel.getX()+10, rocketJLabel.getY());
+                moveRocket(+10,0);
                 break;
         }
     }
+
+    private void moveRocket(int deltaX, int deltaY) {
+        int newX = rocketJLabel.getX() + deltaX;
+        int newY = rocketJLabel.getY() + deltaY;
+
+        int maxX = jFrame.getWidth();
+        int maxY = jFrame.getHeight();
+        System.out.println(newY);
+
+        // Ensure the new position is within the bounds
+        if (newX > 430){
+            newX = 430;
+        }else if (newX < -10){
+            newX = -10;
+        }
+        if (newY > 230){
+            newY = 230;
+        }else if (newY < -160){
+            newY = -160;
+        }
+        rocketJLabel.setLocation(newX, newY);
+    }
+
+
     @Override
     public void keyReleased(KeyEvent e) {
         System.out.println("You released: "+e.getKeyCode());
