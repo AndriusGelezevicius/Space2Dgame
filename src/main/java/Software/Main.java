763fa980b5769c90d86d266a9f4e720063ba78dd
AndriusGelezevicius <andriusgelezevicius@gmail.com>
2023-11-23 18:04:30 +0200

    made a collision and Game Over label

--- src/main/java/Software/Main.java
+++ src/main/java/Software/Main.java
@@ -1,9 +1,11 @@
 package Software;
 
+import java.awt.*;
+
 public class Main {
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FontFormatException {
         MainGui mainGui = new MainGui();
         mainGui.drawingFrame();
     }
--- src/main/java/Software/MainGui.java
+++ src/main/java/Software/MainGui.java
@@ -15,51 +15,62 @@ public class MainGui implements KeyListener {
     JFrame jFrame;
     JLabel rocketJLabel;
     JLabel asteroidJLabel;
+    JLabel gameOverLabel;
     ImageIcon rocketIcon;
     ImageIcon asteroidIcon;
     ImageIcon backgroundImage;
     Timer timer;
     int jFrameWidth = 500;
     int jFrameHeight = 500;
-    int asteroindX = 0;
+    int asteroidX = 0;
     int asteroidY = 0;
     int asteroidXedge = 1;
+    int asteroidYedge = 1;
+    int newX, newY;
 
-    public void drawingFrame(){
+    public void drawingFrame() throws FontFormatException {
+
         jFrame = new JFrame();
         jFrame.setSize(jFrameWidth,jFrameHeight);
         jFrame.setLayout(null);
         jFrame.addKeyListener(this);
         jFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-//        jFrame.setResizable(false);
+        jFrame.setResizable(false);
         jFrame.setLocationRelativeTo(null);
+        Font font = new Font("Monaco", Font.BOLD | Font.ITALIC, 30);
+
         rocketIcon = new ImageIcon("src/rocket (1).png");
         asteroidIcon = new ImageIcon("src/asteroid.png");
-
-
+        gameOverLabel = new JLabel("GAME OVER!");
+        gameOverLabel.setBounds(150,200,200,100);
+        gameOverLabel.setForeground(Color.RED);
+        gameOverLabel.setVisible(false);
+        gameOverLabel.setFont(font);
         rocketJLabel = new JLabel();
-        rocketJLabel.setBounds(210,220,400,400);
+        rocketJLabel.setBounds(210,400,60,65);
         asteroidJLabel = new JLabel();
-        asteroidJLabel.setBounds(asteroindX,asteroidY, 300,300);
+        asteroidJLabel.setBounds(asteroidX,asteroidY, 70,70);
         rocketJLabel.setIcon(rocketIcon);
         asteroidJLabel.setIcon(asteroidIcon);
-
         jFrame.add(asteroidJLabel);
         jFrame.add(rocketJLabel);
+        jFrame.add(gameOverLabel);
 
         jFrame.setVisible(true);
 
         timer = new Timer(100, new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                asteroindX = asteroindX + (10 * asteroidXedge); // Move the asteroid by 10 pixels
+                asteroidX = asteroidX + (10 * asteroidXedge); // Move the asteroid by 10 pixels
 
                 // Check if the asteroid has reached the right edge or left edge
-                if (asteroindX >= 417 || asteroindX <= 0) {
+                if (asteroidX >= 417 || asteroidX <= 0) {
                     asteroidXedge = -asteroidXedge; // Reverse the direction of movement
                 }
 
-                asteroidJLabel.setLocation(asteroindX, asteroidY);
+
+
+                asteroidJLabel.setLocation(asteroidX, asteroidY);
             }
         });
         timer.start();
@@ -88,12 +99,11 @@ public class MainGui implements KeyListener {
     }
 
     private void moveRocket(int deltaX, int deltaY) {
-        int newX = rocketJLabel.getX() + deltaX;
-        int newY = rocketJLabel.getY() + deltaY;
+        newX = rocketJLabel.getX() + deltaX;
+        newY = rocketJLabel.getY() + deltaY;
 
         int maxX = jFrame.getWidth();
         int maxY = jFrame.getHeight();
-        System.out.println(newY);
 
         // Ensure the new position is within the bounds
         if (newX > 430){
@@ -101,19 +111,31 @@ public class MainGui implements KeyListener {
         }else if (newX < -10){
             newX = -10;
         }
-        if (newY > 230){
-            newY = 230;
-        }else if (newY < -160){
-            newY = -160;
+        if (newY > 400){
+            newY = 400;
+        }else if (newY < 0){
+            newY = 0;
+        }
+
+        rocketJLabel.setLocation(newX, newY);
+        System.out.println(newY);
+        checkCollision();
+    }
+    public void checkCollision(){
+        Rectangle rocketRect = new Rectangle(newX, newY, rocketJLabel.getWidth(), rocketJLabel.getHeight());
+        Rectangle asteroidRect = new Rectangle(asteroidX, asteroidY, asteroidJLabel.getWidth(), asteroidJLabel.getHeight());
+
+        if (rocketRect.intersects(asteroidRect)) {
+            System.out.println("GAME OVER");
+            gameOverLabel.setVisible(true);
         }
 
     }
 
 
 
-
     @Override
     public void keyReleased(KeyEvent e) {
-        System.out.println("You released: "+e.getKeyCode());
+
     }
 }
